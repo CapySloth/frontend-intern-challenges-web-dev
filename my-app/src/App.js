3dc8c677fdaa05dfbe70b4ed3184c0e609daee79
karlouski <karlouski@arifleet.ca>
2018-09-18 13:46:06 -0400

    Everything works tandily, have to wrap it up, by storing the favorites in session storate or local storage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,7 +9,7 @@ import PropTypes from 'prop-types';
 const axiosGitHubGraphQL = axios.create({
   baseURL: 'https://api.github.com/graphql',
   headers: {
-    Authorization: 'bearer bbad63ae02ea151edc38a2511ed5d972cfe791ca',
+    Authorization: 'bearer 78d2cf634c8cb19ca7fd3882de3dc1f9033e2cbc',
   },
 });
 
@@ -36,7 +36,53 @@ const getSearchQuery = (query) =>` { search(query: " ${ query } ", type: REPOSIT
     }
   }
 }`
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Content/>
+      </div>
+    )
+  }
+}
+class Content extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorites: []
+    };
 
+    let handleAddFavorite = this.handleAddFavorite.bind(this);
+    let removeFavorite = this.handleRemoveFavorite.bind(this);
+  }
+  handleAddFavorite = (rowToAdd) => {
+    //Pass row information to Favorites, to update set with new information
+    this.setState({
+      favorites: [...this.state.favorites, rowToAdd]
+    });
+    //Is passing the prop more expensive, or the creating of an additional state, that is a smaller value
+    //Put id of row added to the button, so that Add can be remove, great stuff
+  }
+  handleRemoveFavorite = (index) => {
+    var array = [...this.state.favorites]; // make a separate copy of the array
+    console.log(index)
+    array.splice(index, 1);
+    this.setState({ favorites: array });
+    console.log(this.state.favorites)
+    //delete row information from favorites array in favorites component,
+    //delete row id from button control in repositories controller somewhere
+    //Wow, easy pz.
+  }
+	render() {
+		return(
+			<div className="content">
+				<LeftPannel handleAddFavorite={this.handleAddFavorite} favorites={this.state.favorites}/>
+				<RightPannel handleRemoveFavorite={this.handleRemoveFavorite} favorites={this.state.favorites}/>
+			</div>
+		)
+	}
+}
 class Header extends Component {
 	constructor(props) {
  		super(props);
@@ -63,7 +109,7 @@ class LeftPannel extends Component {
 		return (
 			<div className="leftPannel">
 				<Search handleDataUpdate={this.handleDataUpdate.bind(this)}/>
-				<Repositories data={this.state.data}/>
+				<Repositories handleAddFavorite={this.props.handleAddFavorite} favorites={this.props.favorites} data={this.state.data}/>
 			</div>
 		)
 	}
@@ -123,7 +169,7 @@ class Search extends Component {
 class Repositories extends Component {
 	render() {
 		return (
-      <Table data={this.props.data} editType="Add"/>
+      <Table handleAddFavorite={this.props.handleAddFavorite} favorites={this.props.favorites} data={this.props.data} editType="Add"/>
     )
 
 	}
@@ -132,9 +178,9 @@ class Table extends Component {
 	render() {
     let hasData = false;
     let items = [];
+
     if(this.props.data !== null) {
       items = this.props.data;
-      console.log(typeof(items))
       if(items != null) {
         if(items.length !== 0) {
           hasData = true;
@@ -152,9 +198,13 @@ class Table extends Component {
   					</tr>
             {hasData &&
                 items.map(item => <Row
+                handleAddFavorite={this.props.handleAddFavorite}
+                handleRemoveFavorite={this.props.handleRemoveFavorite}
+                favorites={this.props.favorites}
+                node={item}
                 key={item.node.id}
                 link={item.node.url}
-                name={item.node.nameWithOwner}
+                nameWithOwner={item.node.nameWithOwner}
                 language={(item.node.primaryLanguage === null) ? "-" : item.node.primaryLanguage.name }
                 tag={(item.node.tags.edges.length === 0) ? "-" : item.node.tags.edges[0].tag.name}
                 editType={this.props.editType}/>)
@@ -166,71 +216,71 @@ class Table extends Component {
 		)
 	}
 }
-
+//Fix edit button and button eddit stuff, ugly :(
 class Row extends Component {
+  handleClick = (event, editType) => {
+    if(editType === "Add") {
+      this.props.handleAddFavorite(this.props.node);
+    } else if(editType === "Remove") {
+      console.log(this.props.nameWithOwner)
+      this.props.handleRemoveFavorite(this.props.favorites.indexOf(this.props.node));
+    }
+  }
 	render() {
-		return (
-			<tr>
-				<td><a href={this.props.link}>{this.props.name}</a></td>
-				<td>{this.props.language}</td>
-				<td>{this.props.tag}</td>
-				<td className="edit-button"><RowMutationButton editType= {this.props.editType}/></td>
-			</tr>
-		)
-	}
-}
-class RowMutationButton extends Component {
-	constructor(props) {
- 		super(props);
-	}
-	render() {
-		return(
-			<button className="button-edit">{ this.props.editType }</button>
-		)
+    let found = 0;
+    if(this.props.favorites.length !== 0) {
+      found = this.props.favorites.some(item => item.node.nameWithOwner === this.props.nameWithOwner);
+    }
+    if(found && this.props.editType === "Add") {
+      return (
+  			<tr>
+  				<td><a href={this.props.link}>{this.props.nameWithOwner}</a></td>
+  				<td>{this.props.language}</td>
+  				<td>{this.props.tag}</td>
+  				<td className="edit-button"></td>
+  			</tr>
+  		)
+    } else {
+      return (
+      <tr>
+        <td><a href={this.props.link}>{this.props.nameWithOwner}</a></td>
+        <td>{this.props.language}</td>
+        <td>{this.props.tag}</td>
+        <td className="edit-button"><button className="button-edit" onClick={(event) => this.handleClick(event, this.props.editType)}>{this.props.editType}</button></td>
+      </tr>
+      )
+    }
 	}
 }
 /* <Favorites data={this.state.favorites}/>*/
 class RightPannel extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      favorites: []
-    };
-    //console.log(this.state.favorites);
+    this.state= { data : [] }
+  }
+  componentWillReceiveProps(nextProps) {
+    if(nextProps != null) {
+      this.setState({ data: nextProps.favorites})
+    }
   }
 	render() {
-		return(
-			<div className="rightPannel">
-        <Favorites data={this.state.favorites}/>
-			</div>
-		)
+    console.log(this.state.data.length);
+    if(this.state.data.length !== 0 && this.state.data != null) {
+      return(
+  			<div className="rightPannel">
+          <Favorites handleRemoveFavorite={this.props.handleRemoveFavorite} data={this.state.data}/>
+  			</div>
+  		)
+    } else {
+      return(
+        <div className="rightPannel"></div>
+      )
+    }
 	}
 }
 class Favorites extends Component {
 	render() {
-		return <Table data={this.props.favorites} editType="Remove"/>
+		return <Table data={this.props.data} handleRemoveFavorite={this.props.handleRemoveFavorite} favorites={this.props.data} editType="Remove"/>
 	}
 }
-class Content extends Component {
-	render() {
-		return(
-			<div className="content">
-				<LeftPannel/>
-				<RightPannel/>
-			</div>
-		)
-	}
-}
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header/>
-        <Content/>
-      </div>
-    )
-  }
-}
-
 export default App;
